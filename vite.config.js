b1b716ac458fa5eceef30d6482113ffe433e4a41
kashyapprajapat <prajapatikashyap14@gmail.com>
2025-03-27 13:35:37 +0530

    Prod:create vercel.json file for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),],
-  base:"/password-generater/"
+  plugins: [react(), tailwindcss()],
+  base: "/password-generater/"
 })
